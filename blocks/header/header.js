ae36eeca940ebfd28acb12cfe5806fed2ceb7096
Kyle Buchanan <kylebuch8@gmail.com>
2025-09-17 14:29:08 -0400

    adding rh-avatar and rh-button to the header.js block

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -2,6 +2,8 @@ import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 import '@rhds/elements/rh-navigation-primary/rh-navigation-primary.js';
+import '@rhds/elements/rh-avatar/rh-avatar.js';
+import '@rhds/elements/rh-button/rh-button.js';
 
 // media query match that indicates mobile/tablet width
 const isDesktop = window.matchMedia('(min-width: 900px)');
