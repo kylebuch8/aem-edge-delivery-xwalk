445f3d1e85394adbd71439f608ddf27b2d5fef03
Kyle Buchanan <kylebuch8@gmail.com>
2025-09-18 10:37:31 -0400

    adding more fields to tile

--- blocks/tile/tile.js
+++ blocks/tile/tile.js
@@ -1,5 +1,4 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
-
 import '@rhds/elements/rh-tile/rh-tile.js';
 
 export default function decorate(block) {
@@ -9,6 +8,10 @@ export default function decorate(block) {
     moveInstrumentation(child, tile);
     while (child.firstElementChild.firstElementChild) {
       tile.append(child.firstElementChild.firstElementChild);
+    }
+
+    if (tile.querySelector('h1, h2, h3, h4, h5, h6')) {
+      tile.querySelector('h1, h2, h3, h4, h5, h6').setAttribute('slot', 'headline');
     }
   });
 
